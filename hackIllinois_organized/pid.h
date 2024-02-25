a56dd455ef3655c073a38be29b7ad8c5f5f23d50
isaacangyu <86135733+isaacangyu@users.noreply.github.com>
2024-02-25 09:07:59 -0600

    Update pid.h

--- hackIllinois_organized/pid.h
+++ hackIllinois_organized/pid.h
@@ -4,7 +4,7 @@
 
 const float kP = 1;    // find through experimentation
 const float kI = 0;  // set to 0 for now
-const float kD = 10;     // find through experimentation  //decrease to 40 helped
+const float kD = 15;     // find through experimentation
 const uint8_t kMaxSpeed = 105; //old was 105
 const int8_t kMinSpeed = -105; //old was 105
 const uint8_t kBaseSpeed = 100;
